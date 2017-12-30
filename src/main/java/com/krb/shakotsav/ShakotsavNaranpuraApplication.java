1053bc75f4715483b13fcf22998e109b3832d099
ketan Bhavsar <ketan@DESKTOP-NVRKIE8>
2017-12-30 21:59:24 +0530

    Adding Commit

--- src/main/java/com/krb/shakotsav/ShakotsavNaranpuraApplication.java
+++ src/main/java/com/krb/shakotsav/ShakotsavNaranpuraApplication.java
@@ -2,11 +2,17 @@ package com.krb.shakotsav;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class ShakotsavNaranpuraApplication {
-
+public class ShakotsavNaranpuraApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ShakotsavNaranpuraApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ShakotsavNaranpuraApplication.class, args);
 	}
